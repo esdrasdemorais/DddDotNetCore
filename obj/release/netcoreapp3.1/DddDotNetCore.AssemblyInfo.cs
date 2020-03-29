4bda102f9943e3fa6c011c1e70fbe0afbd289170
Esdras de Morais da Silva <esdrasdemorais@gmail.com>
2020-03-28 23:23:57 -0700

    Named and Versioned Package

--- obj/release/netcoreapp3.1/DddDotNetCore.AssemblyInfo.cs
+++ obj/release/netcoreapp3.1/DddDotNetCore.AssemblyInfo.cs
@@ -10,7 +10,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("DddDotNetCore")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Esdras de Morais IT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
